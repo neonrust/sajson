f2839fda6bf055eac391a29271bdddc344867900
André Jonsson <andre.jonsson@dataductus.se>
2022-04-27 20:40:58 +0200

    Improved string escaping.
    Now also escapes \t
    Should arguably using some already established/tested/correct code for this...

--- include/sajson_dump.h
+++ include/sajson_dump.h
@@ -1,19 +1,47 @@
+/*
+ * Copyright (c) 2012-2017 Chad Austin
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #pragma once
 
 #include "sajson.h"
 
+#include <cstdio>
 #include <string_view>
 using namespace std::literals;
 
+/**
+ * Default implementation for writing to a file.
+ */
 struct FileOut
 {
-    inline FileOut(FILE *f) : _fp(f), _size(0) {}
+    inline FileOut(std::FILE *f) : _fp(f), _size(0) {}
     inline FileOut &operator += (std::string_view s) {
-        _size += fwrite(s.data(), 1, s.size(), _fp);
+        _size += std::fwrite(s.data(), 1, s.size(), _fp);
         return *this;
     }
     inline FileOut &operator += (char c) {
-        _size += fwrite(&c, 1, 1, _fp);
+        _size += std::fwrite(&c, 1, 1, _fp);
         return *this;
     }
     inline size_t size() const {
@@ -21,31 +49,12 @@ struct FileOut
     }
 
 private:
-    FILE *_fp { nullptr };
+    std::FILE *_fp { nullptr };
     size_t _size {0 };
 };
 
 namespace sajson
 {
-
-inline std::string escape(std::string_view s) {
-    std::string escaped;
-    escaped.reserve(s.size() + s.size()/10); // make initial space to 10% double quotes, which seems like a lot ;)
-    size_t pos { 0 };
-    while(true) {
-        auto n = s.find_first_of("\"\\", pos);
-        if(n == std::string_view::npos)
-        {
-            escaped += s.substr(pos);
-            break;
-        }
-        escaped += s.substr(pos, n - pos);
-        escaped += "\\"sv;
-        escaped += s[n];
-        pos = n + 1; // continue after what was copied (plus the found char)
-    }
-    return escaped;
-}
 
 template<typename O>
 inline void dump(O &o, const sajson::value &value, int indent=-1);
@@ -56,15 +65,38 @@ namespace internal
 
 template<typename O>
 inline void ind(O &o, int n) {
-    static const char i_str[] = "  ";
+    static constexpr char i_str[] = "  ";
 
     while(n-- > 0) {
         o += i_str;
     }
 }
 
+
 template<typename O>
-inline void dump_array(O &o, const sajson::array &arr, int indent=-1) {
+inline void dump_string(O &o, std::string_view s) {
+    o += '"';
+    size_t pos { 0 };
+    while(true) {
+        auto n = s.find_first_of("\"\n\t\\", pos);
+        if(n == std::string_view::npos)
+        {
+            o += s.substr(pos);
+            break;
+        }
+        o += s.substr(pos, n - pos);
+        o += "\\"sv;
+        if(s[n] == '\n' || s[n] == '\t')
+            o += 'n';
+        else
+            o += s[n];
+        pos = n + 1; // continue after what was copied (plus the found char)
+    }
+    o += '"';
+}
+
+template<typename O>
+inline void dump_array(O &o, const sajson::array &arr, int indent) {
     o += '[';
 
     if(indent != -1)
@@ -73,11 +105,12 @@ inline void dump_array(O &o, const sajson::array &arr, int indent=-1) {
     size_t idx = 0;
     for(const auto elem: arr)
     {
-        if(indent > 0)
-        {
+        if(indent != 0)
             o += '\n';
+        if(indent > 0)
             ind(o, indent);
-        }
+
+        // output the array element
         dump(o, elem, indent);
 
         ++idx;
@@ -96,7 +129,7 @@ inline void dump_array(O &o, const sajson::array &arr, int indent=-1) {
 }
 
 template<typename O>
-inline void dump_object(O &o, const sajson::object &obj, int indent=-1) {
+inline void dump_object(O &o, const sajson::object &obj, int indent) {
     o += '{';
 
     if(indent != -1)
@@ -105,19 +138,20 @@ inline void dump_object(O &o, const sajson::object &obj, int indent=-1) {
     size_t idx = 0;
     for(const auto [key, value]: obj)
     {
-        if(indent > 0)
-        {
+        if(indent != 0)
             o += '\n';
+        if(indent > 0)
             ind(o, indent);
-        }
 
+        // output the key
         o += '"';
         o += key;
         o += '"';
         o += ':';
-        if(indent >= 0)
+        if(indent != 0)
             o += ' ';
 
+        // output the value
         dump(o, value, indent);
 
         ++idx;
@@ -137,6 +171,12 @@ inline void dump_object(O &o, const sajson::object &obj, int indent=-1) {
 
 } // NS: internal
 
+/**
+ * Dump a sajson::value to the specified output, 'o'.
+ * If 'indent' is >= 0, LF and indentation will be used.
+ * Otherwise, everything will be written on a single line, with no spaces.
+ * Indentation size can currently not be controlled.
+ */
 template<typename O>
 inline void dump(O &o, const sajson::value &value, int indent) {
     switch(value.get_type())
@@ -157,9 +197,7 @@ inline void dump(O &o, const sajson::value &value, int indent) {
         o += "false";
         break;
     case TYPE_STRING:
-        o += '"';
-        o += escape(value.as_string());  // TODO: escape double-quotes (")
-        o += '"';
+        internal::dump_string(o, value.as_string());
         break;
     case TYPE_ARRAY:
         internal::dump_array(o, value.as_array(), indent);
@@ -170,6 +208,12 @@ inline void dump(O &o, const sajson::value &value, int indent) {
     }
 }
 
+/**
+ * Serialize a sajson::value to a string.
+ * If 'indent' is true, LF and indentation will be used.
+ * Otherwise, everything will be written on a single line, with no spaces.
+ * Indentation size can currently not be controlled.
+ */
 inline std::string to_string(const value &value, bool indent=false) {
 
     std::string so;
@@ -180,7 +224,13 @@ inline std::string to_string(const value &value, bool indent=false) {
     return so;
 }
 
-inline size_t write(FILE *f, const value &value, bool indent=false) {
+/**
+ * Serialize a sajson::value to the given file pointer.
+ * If 'indent' is true, LF and indentation will be used.
+ * Otherwise, everything will be written on a single line, with no spaces.
+ * Indentation size can currently not be controlled.
+ */
+inline size_t write(std::FILE *f, const value &value, bool indent=false) {
     FileOut fo(f);
 
     dump(fo, value, indent? 0: -1);
@@ -188,5 +238,23 @@ inline size_t write(FILE *f, const value &value, bool indent=false) {
     return fo.size();
 }
 
+/**
+ * Serialize a sajson::value to the given file path.
+ * If 'indent' is true, LF and indentation will be used.
+ * Otherwise, everything will be written on a single line, with no spaces.
+ * Indentation size can currently not be controlled.
+ */
+inline size_t write(const std::string &filepath, const value &value, bool indent=false) {
+    std::FILE *f = std::fopen(filepath.c_str(), "wb");
+    if(f == nullptr) {
+        return static_cast<size_t>(-1);
+    }
+
+    auto written = write(f, value, indent);
+
+    std::fclose(f);
+
+    return written;
+}
 
 }
